7427475225e3ef95ee993eb05c3bfb3ce3d5dfff
Anton Rudenko <Paravozzz@users.noreply.github.com>
2023-11-12 21:31:47 +0300

    Issue 6 (#7)
    * Продолжение работы.
    
    * Исправлена ошибка, при которой не создавался мастер счёт при добавлении валюты в список валют пользователя.

--- src/main/java/ru/homebuh/core/service/UserInfoService.java
+++ src/main/java/ru/homebuh/core/service/UserInfoService.java
@@ -5,10 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
+import ru.homebuh.core.controller.dto.MasterAccountCreate;
 import ru.homebuh.core.controller.dto.UserInfoCreate;
+import ru.homebuh.core.domain.AccountEntity;
 import ru.homebuh.core.domain.CurrencyEntity;
 import ru.homebuh.core.domain.UserInfoEntity;
+import ru.homebuh.core.mapper.AccountMapper;
 import ru.homebuh.core.mapper.UserInfoMapper;
+import ru.homebuh.core.repository.AccountRepository;
 import ru.homebuh.core.repository.CurrencyRepository;
 import ru.homebuh.core.repository.UserInfoRepository;
 import ru.homebuh.core.util.Constatnts;
@@ -21,7 +25,9 @@ import java.util.Optional;
 public class UserInfoService {
     private final UserInfoRepository userInfoRepository;
     private final CurrencyRepository currencyRepository;
+    private final AccountRepository accountRepository;
     private final UserInfoMapper mapper;
+    private final AccountMapper accountMapper;
 
     @Transactional
     public UserInfoEntity create(UserInfoCreate userInfoCreate) {
@@ -44,6 +50,9 @@ public class UserInfoService {
         if (!userInfoEntity.getCurrencies().contains(currencyEntity)) {
             userInfoEntity.getCurrencies().add(currencyEntity);
             userInfoRepository.save(userInfoEntity);
+
+            AccountEntity masterAccountEntity = accountMapper.map(new MasterAccountCreate(currencyEntity.getCode(), userInfoEntity.getId()));
+            accountRepository.save(masterAccountEntity);
         }
 
         return currencyEntity;
